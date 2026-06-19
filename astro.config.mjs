87cd04612e1446ee05fc6f404b32d7ea7b879132
GooCampus <info@goocampus.in>
2026-06-20 00:00:51 +0530

    Set Astro base to / so Netlify serves from root (goocampusevents.com proxy strips /nz-pathway prefix)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://goocampusevents.com',
-  base: '/nz-pathway',
+  base: '/',
   compressHTML: true,
 });
